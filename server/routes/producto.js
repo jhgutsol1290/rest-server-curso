4a40df20f75073e9c417387beba4c1a65092ddf9
jh <dev1@correo.com>
2019-06-03 17:52:57 -0500

    API con buscador

--- server/routes/producto.js
+++ server/routes/producto.js
@@ -9,18 +9,98 @@ const Producto = require('../models/producto')
 //---------------------------
 //Obtener todos los productos
 //---------------------------
-app.get('/productos', (req, res)=>{
+app.get('/producto', verificaToken, (req, res)=>{
     //trae todos los productos
     //populate: usuario categoria
     //paginado
+    let desde = req.query.desde || 0
+    desde = Number(desde)
+    let hasta = req.query.hasta || 5
+    hasta = Number(hasta)
+
+    Producto.find({disponible: true})
+                    .populate('usuario', 'nombre email')
+                    .populate('categoria', 'descripcion')
+                    .skip(desde)
+                    .limit(hasta)
+                    .exec((err, productosDB)=>{
+                        if(err){
+                            return res.send(500).json({
+                                ok: false,
+                                err
+                            })
+                        }
+
+                        return res.json({
+                            ok: true,
+                            productos: productosDB
+                        })
+
+                    })
 })
 
 //---------------------------
 //Obtener un porducto por ID
 //---------------------------
-app.get('/productos/:id', (req, res)=>{
+app.get('/producto/:id', verificaToken, (req, res)=>{
     //populate: usuario categoria
     //paginado
+    const id = req.params.id
+
+    Producto.findById(id)
+                .populate('usuario', 'nombre email')
+                .populate('categoria', 'descripcion')
+                .exec((err, productoDB)=>{
+                    if(err){
+                        return res.status(500).json({
+                            ok: false,
+                            err
+                        })
+                    }
+
+                    if(!productoDB){
+                        return res.status(400).json({
+                            ok: false,
+                            err:{
+                                message: 'ID inexistente'
+                            }
+                        })
+                    }
+
+
+                    return res.json({
+                        ok: true,
+                        producto: productoDB
+                    })
+                })
+
+})
+
+//---------------------------
+//Buscar producto
+//---------------------------
+app.get('/producto/buscar/:termino', verificaToken, (req, res)=>{
+
+    let termino = req.params.termino
+
+    let regex = new RegExp(termino, 'i')
+
+    Producto.find({nombre: regex})
+            .populate('categoria', 'descripcion')
+            .exec((err,productosDB)=>{
+                if(err){
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+
+                res.json({
+                    ok: true,
+                    productos: productosDB
+                })
+            })
+
 })
 
 //---------------------------
@@ -70,7 +150,7 @@ app.post('/producto', verificaToken, (req, res)=>{
 //---------------------------
 //Actualizar un porducto por ID
 //---------------------------
-app.put('/producto/:id', (req, res)=>{
+app.put('/producto/:id', verificaToken, (req, res)=>{
     //grabar el usuario
     //grabar una categoria del listado
 
@@ -86,7 +166,7 @@ app.put('/producto/:id', (req, res)=>{
         }
 
         if(!productoDB){
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 err:{
                     message: 'ID no existente'
@@ -119,7 +199,7 @@ app.put('/producto/:id', (req, res)=>{
 //---------------------------
 //Eliminar un porducto por ID
 //---------------------------
-app.delete('/producto/:id', (req, res)=>{
+app.delete('/producto/:id', verificaToken, (req, res)=>{
     //cambiar estado de disponible a false
     const id = req.params.id
 
@@ -132,7 +212,7 @@ app.delete('/producto/:id', (req, res)=>{
         }
 
         if(!productoDB){
-            return res.status(500).json({
+            return res.status(400).json({
                 ok: false,
                 err:{
                     message: 'ID no existente'
@@ -140,7 +220,11 @@ app.delete('/producto/:id', (req, res)=>{
             })
         }
 
-        productoDB.disponible = false
+        if(productoDB.disponible === true){
+            productoDB.disponible = false
+        } else {
+            productoDB.disponible = true
+        }
 
         productoDB.save((err, productoInactivo)=>{
             if(err){
